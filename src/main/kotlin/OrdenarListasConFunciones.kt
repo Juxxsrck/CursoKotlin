f921de752ee2adfb2d466c6a9c6c6de879fe7e9c
juxxsrck <justine.pulido@gmail.com>
2022-08-16 21:16:09 -0500

    Como ordenar listas con las funciones que tiene Kotlin

---
+++ src/main/kotlin/OrdenarListasConFunciones.kt
@@ -0,0 +1,24 @@
+/*
+val numerosDeLoteria = listOf(11, 22, 43, 56, 78, 66)
+
+    val numeroSorted = numerosDeLoteria.sorted()
+    println(numeroSorted)
+
+    val numerosLoteriaDecendiente = numerosDeLoteria.sortedDescending()
+    println(numerosLoteriaDecendiente)
+
+    val ordenarPorMultiplos = numerosDeLoteria.sortedBy { numero -> numero < 50 }
+    println(ordenarPorMultiplos)
+
+    val numerosAleatorios = numerosDeLoteria.shuffled()
+    println(numerosAleatorios)
+
+    val numerosEnReversa = numerosDeLoteria.reversed()
+    println(numerosEnReversa)
+
+    val mensajesDeNumeros = numerosDeLoteria.map { numero -> "Tu numero de loteria es $numero" }
+    println(mensajesDeNumeros)
+
+    val numerosFiltrados = numerosDeLoteria.filter { numero -> numero > 50 }
+    println(numerosFiltrados)
+ */
