b66ca4c5320322f43161ccbe0ce3de9fa6306e3f
juxxsrck <justine.pulido@gmail.com>
2022-08-23 23:10:39 -0500

    Sets

---
+++ src/main/kotlin/Sets.kt
@@ -0,0 +1,16 @@
+/*
+val vocalesRepetidas = setOf("a", "e", "i", "o", "u", "a", "e", "i", "o", "u")
+    println(vocalesRepetidas)
+
+    val numerosFavoritos = mutableSetOf(1,2,3,4)
+    println(numerosFavoritos)
+    numerosFavoritos.add(5)
+    println(numerosFavoritos)
+
+    numerosFavoritos.remove(5)
+    println(numerosFavoritos)
+
+    val valorDelSet = numerosFavoritos.firstOrNull { numero -> numero > 2 }
+    println(valorDelSet)
+
+ */
