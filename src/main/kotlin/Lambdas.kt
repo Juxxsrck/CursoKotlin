b82fc859547be46203caec07006aedd184b6a960
juxxsrck <justine.pulido@gmail.com>
2022-08-24 22:57:25 -0500

    Parametros de funciones, lambdas en Kotlin

---
+++ src/main/kotlin/Lambdas.kt
@@ -0,0 +1,9 @@
+/*
+val myLambda : (String) -> Int = { valor -> valor.length }
+    val lambdaEjecutada = myLambda("Hola Platzi")
+    println(lambdaEjecutada)
+
+    val saludos = listOf("hello","hola","ciao")
+    val lonngitudDeSaludos = saludos.map(myLambda)
+    println(lonngitudDeSaludos)
+ */
---
+++ src/main/kotlin/PrametrosFunciones.kt
@@ -0,0 +1,10 @@
+/*
+fun main(args: Array<String>) {
+    imprimirNombre(nombre = "Justine",  apellido = "Pulido")
+}
+
+fun imprimirNombre (nombre : String, segundoNombre : String = "",  apellido : String) {
+    //println("Mi nombre es $nombre y mi apellido es $apellido")
+    println("Mi nombre completo es $nombre $segundoNombre $apellido")
+}
+ */
